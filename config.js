5ddb7c89e6102eb781739662f1b8cb6630a76026
Hades491 <zaidiabbasraza906@gmail.com>
2026-09-11 17:00:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Paste the Web App URL you get after deploying backend/Code.gs here.
 // It looks like: https://script.google.com/macros/s/XXXXXXXX/exec
 // ------------------------------------------------------------------
-const API_URL = "https://script.google.com/macros/s/AKfycbyNHj0f1FyEiyT8LxLkQvHzznDJkitpjqLdGqWqHd0mGtFRnrAMLPInHUfrnzEJcQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyMM26RFmFll59f8KlJ7hpHIHi0NbNPWtxIPb5gfX-tuHPVdMrL3ZfFSp09LqQ6UGOO/exec";
